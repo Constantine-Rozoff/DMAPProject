70d57760977417946e0e99456adaa00fa131ef53
Constantine-Rozoff <rozov888@gmail.com>
2022-03-23 16:35:51 +0200

    Dashboard variables and main class start settings

--- src/main/java/DashboardPage.java
+++ src/main/java/DashboardPage.java
@@ -1,9 +1,16 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class DashboardPage {
-    WebDriver driver;
+    private WebDriver driver;
 
     public DashboardPage(WebDriver driver) {
         this.driver = driver;
     }
+
+    private By dmapLogo = By.xpath("//div[@class='sidebar-logo-item']");
+    private By sideBarCampaigns = By.xpath("//div[@class='sidebar-item']/label[(text()='Campaigns')]");
+    private By subMenuCampaigns = By.xpath("//div[@class='sidebar-item']//a[@href='/campaigns']");
+    private By sideBarData = By.xpath("//div[@class='sidebar-item']/label[(text()='Data')]");
+    private By subMenuAudiences = By.xpath("//div[@class='sidebar-item']//a[@href='/data/audiences']");
 }
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,9 +1,19 @@
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainClass {
-    private WebDriver driver;
+    static WebDriver driver;
 
     public static void main(String[] args) {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("https://qa.dmap.cloud/auth/login");
 
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.signInToTheSystem("danielle", "monsterPass123!");
     }
 }
